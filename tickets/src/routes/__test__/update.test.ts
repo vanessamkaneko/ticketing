f086c06f25eba8d8b99b5b9a3d61db6b80c2ac2a
Vanessa Kaneko <vanessa.mitiek@gmail.com>
2024-06-27 00:08:26 -0300

    studying

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -23,12 +23,12 @@ it('returns a 401 if the user is not authenticated', async () => {
       price: 20
     })
     .expect(401)
-  
+
 })
 
 it('returns a 401 if the user does not own the ticket', async () => {
   const response = await request(app)
-    .post('/api/tickets/')
+    .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
       title: 'kdlskdl',
@@ -43,13 +43,64 @@ it('returns a 401 if the user does not own the ticket', async () => {
       price: 1000
     })
     .expect(401);
-  
+
 })
 
 it('returns a 400 if the user provides an invalid title or price', async () => {
-  
+  const cookie = global.signin()
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'kdlskdl',
+      price: 20
+    })
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: '',
+      price: 20
+    })
+    .expect(400)
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'aaaaaaa',
+      price: -20
+    })
+    .expect(400)
+
 })
 
 it('updates the ticket provided valid inputs', async () => {
-  
+  const cookie = global.signin()
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'kdlskdl',
+      price: 20
+    })
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'New title',
+      price: 100
+    })
+    .expect(200)
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketResponse.body.title).toEqual('New title');
+  expect(ticketResponse.body.price).toEqual(100);
 })
