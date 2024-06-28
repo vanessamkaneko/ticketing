98486636f22b86ee697ef5836c86eab44aada281
Vanessa Kaneko <vanessa.mitiek@gmail.com>
2024-06-28 11:59:15 -0300

    studying

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 
 console.clear();
@@ -33,10 +33,56 @@ stan.on('connect', () => {
       console.log(`RECEIVED message (event) #${msg.getSequence()}, with data: ${data}!`)
     }
 
-    msg.ack(); /* aqui, uma vez que o evento foi recebido, o processo termina (s/ ele, o evento ficaria sendo reenviado a cada 30s
-    p/ os membros do queue group) */
+    msg.ack(); /* aqui, uma vez que o evento foi recebido, o processo termina (s/ ele, o evento ficaria sendo reenviado a cada 30s (default)
+    do .setManualAckMode p/ os membros do queue group) */
   })
 })
 
 process.on('SIGINT', () => stan.close())
 process.on('SIGTERM', () => stan.close())
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
+  private client: Stan;
+  protected ackWait = 5 * 1000;
+
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on('message', (msg: Message) => {
+      console.log(
+        `Message received: ${this.subject} / ${this.queueGroupName}`
+      );
+
+      const parsedData = this.parseMessage(msg)
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8')) //parse a Buffer e get a JSON from it
+  }
+}
