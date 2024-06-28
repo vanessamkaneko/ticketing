fb4ffc043741849bb74e86ef81f55c6d0f3460d7
Vanessa Kaneko <vanessa.mitiek@gmail.com>
2024-06-28 00:46:48 -0300

    studying

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,15 +1,30 @@
 import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222'
 });
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS')
 
-  const subscription = stan.subscribe('ticket:created');
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  })
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    /* setManualAckMode(true) é p/ garantir o processamento completo do evento recebido, caso não ocorra, terá algum erro e o NATS tentará
+    reenviar o evento p/ reprocessamento a fim dele não se perder no caminho*/
+    .setDeliverAllAvailable()
+    .setDurableName('accounting-service');
+
+  const subscription = stan.subscribe('ticket:created', 'queue-group-name', options);
+  // 1º nome do channel que quer "escutar" | nome do queue group
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
@@ -17,6 +32,11 @@ stan.on('connect', () => {
     if (typeof data === 'string') {
       console.log(`RECEIVED message (event) #${msg.getSequence()}, with data: ${data}!`)
     }
-  })
 
+    msg.ack(); /* aqui, uma vez que o evento foi recebido, o processo termina (s/ ele, o evento ficaria sendo reenviado a cada 30s
+    p/ os membros do queue group) */
+  })
 })
+
+process.on('SIGINT', () => stan.close())
+process.on('SIGTERM', () => stan.close())
