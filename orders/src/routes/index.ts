57eeecc261efac571d6e2f5ba422f186e6a2557f
Vanessa Kaneko <vanessa.mitiek@gmail.com>
2024-07-05 23:39:13 -0300

    studying

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,15 @@
+import { requireAuth } from '@vmktickets/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-  res.send({})
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id
+  }).populate('ticket') // p/ retornar infos do ticket (title e price) que essa order se refere; senão só retorna o id do ticket
+  
+  res.send(orders)
 })
 
 export { router as indexOrderRouter }
