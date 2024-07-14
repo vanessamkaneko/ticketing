8e20c52e0a9a2f5ad38262d4c2a2793ce2fbe686
Vanessa Kaneko <vanessa.mitiek@gmail.com>
2024-07-13 22:46:20 -0300

    studying

---
+++ payments/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser'; // basicamente: converte body da requisição em json
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@vmktickets/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, //desabilitando encriptação no cookie (pois pode causar conflitos dependendo da linguagem utilizada)
+    secure: process.env.NODE_ENV !== 'test' /* sempre que forem rodados testes jest no terminal, essa variável de node environment será 
+    igual a test (retornando false, possibilitando o envio de cookie mesmo em requisições http); por outro lado, se estivermos em outro 
+    environment (development ou production), o secure retornará true  */
+
+    /* secure: true // cookies serão compartilhados apenas p/ requests c/ conexão HTTPS --> por estarmos utilizando o Supertest e ele não
+    fazer requisições https (e sim http), tivemos que mudar o valor do secure p/ o código de autenticação funcionar mesmo em ambiente
+    de testes */
+  })
+)
+app.use(currentUser);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+})
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,66 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+/* iniciará uma cópia do mongodb na memória, permitindo rodar vários testes ao mesmo tempo de diferentes projetos sem que eles tentem se
+conectar a instância do mongo de uma só vez. Mongo memory também nos dá acesso direto à memória/mongodb*/
+import mongoose from 'mongoose';
+import { app } from '../app';
+import request from 'supertest';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  var signin: () => string[];
+}
+
+jest.mock('../nats-wrapper') /*dizendo ao jest o caminho do arquivo original que queremos fazer um fake (o fake precisa ter o mesmo nome do original pois é c/ base nele
+que o doc será executado na pasta __mocks__) -> fazendo conexão fake c/ o nats p/ realizar testes */
+
+// função que irá rodar antes de todos os testes serem executados
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = 'sdffadsf';
+
+  mongo = await MongoMemoryServer.create(); // criando mongo memory
+  const mongoUri = mongo.getUri(); // tentando obter a uri do mongo memory p/ se conectar
+
+  await mongoose.connect(mongoUri, {})
+})
+
+// irá rodar antes de cada teste
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections(); // nos dá todas as coleções que existem no database
+
+  for (let collection of collections) { // irá fazer o loop em toda as coleções e deletar todos os dados de dentro deles (resetar todos os dados)
+    await collection.deleteMany({})
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  // Build a JWT payload { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com'
+  };
+
+  // Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session object { jwt: MY_JWT }
+  const session = { jwt: token }
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session)
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
+}
+
